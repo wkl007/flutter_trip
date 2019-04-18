2a4e11b63262d4627711d36b9ae40223145fa57f
wkl--007 <499657357@qq.com>
2019-04-18 22:23:33 +0800

    tabNavigator实现

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './navigator/tab_navigator.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,54 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: TabNavigator(),
     );
   }
 }
---
+++ lib/navigator/tab_navigator.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import '../pages/home_page.dart';
+import '../pages/travel_page.dart';
+import '../pages/search_page.dart';
+import '../pages/my_page.dart';
+
+class TabNavigator extends StatefulWidget {
+  @override
+  _TabNavigatorState createState() => _TabNavigatorState();
+}
+
+class _TabNavigatorState extends State<TabNavigator> {
+  final _defaultColor = Colors.grey;
+  final _activeColor = Colors.blue;
+  var _controller = PageController(initialPage: 0);
+  int _currentIndex = 0;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _controller,
+        children: <Widget>[
+          HomePage(),
+          SearchPage(),
+          TravelPage(),
+          MyPage(),
+        ],
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          selectedFontSize: 12,
+          unselectedFontSize: 12,
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            _controller.jumpToPage(index);
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: _defaultColor,
+                ),
+                activeIcon: Icon(
+                  Icons.home,
+                  color: _activeColor,
+                ),
+                title: Text(
+                  '首页',
+                  style: TextStyle(
+                      color: _currentIndex != 0 ? _defaultColor : _activeColor),
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                  color: _defaultColor,
+                ),
+                activeIcon: Icon(
+                  Icons.search,
+                  color: _activeColor,
+                ),
+                title: Text(
+                  '搜索',
+                  style: TextStyle(
+                      color: _currentIndex != 1 ? _defaultColor : _activeColor),
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.camera_alt,
+                  color: _defaultColor,
+                ),
+                activeIcon: Icon(
+                  Icons.camera_alt,
+                  color: _activeColor,
+                ),
+                title: Text(
+                  '旅拍',
+                  style: TextStyle(
+                      color: _currentIndex != 2 ? _defaultColor : _activeColor),
+                )),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.account_circle,
+                  color: _defaultColor,
+                ),
+                activeIcon: Icon(
+                  Icons.account_circle,
+                  color: _activeColor,
+                ),
+                title: Text(
+                  '我的',
+                  style: TextStyle(
+                      color: _currentIndex != 3 ? _defaultColor : _activeColor),
+                )),
+          ]),
+    );
+  }
+}
