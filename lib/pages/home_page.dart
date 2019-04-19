9ae0400f296438afb3a0280e7256461693d1e48b
wkl--007 <499657357@qq.com>
2019-04-19 22:31:57 +0800

    轮播图Banner与自定义AppBar实现滚动渐变

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+const APPBAR_SCROLL_OFFSET = 100;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,12 +9,109 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _imageUrls = [
+    'http://www.devio.org/io/flutter_app/img/banner/100h10000000q7ght9352.jpg',
+    'https://dimg04.c-ctrip.com/images/300h0u000000j05rnD96B_C_500_280.jpg',
+    'http://pages.ctrip.com/hotel/201811/jdsc_640es_tab1.jpg',
+    'https://dimg03.c-ctrip.com/images/fd/tg/g1/M03/7E/19/CghzfVWw6OaACaJXABqNWv6ecpw824_C_500_280_Q90.jpg'
+  ];
+
+  double appBarAlpha = 0;
+
+  List _bannerList = [
+    {
+      "icon":
+          "http://www.devio.org/io/flutter_app/img/banner/100h10000000q7ght9352.jpg",
+      "url":
+          "https://gs.ctrip.com/html5/you/travels/1422/3771516.html?from=https%3A%2F%2Fm.ctrip.com%2Fhtml5%2F"
+    },
+    {
+      "icon":
+          "https://dimg04.c-ctrip.com/images/300h0u000000j05rnD96B_C_500_280.jpg",
+      "url":
+          "https://m.ctrip.com/webapp/vacations/tour/detail?productid=3168213&departcityid=2&salecityid=2&from=https%3A%2F%2Fm.ctrip.com%2Fhtml5%2F"
+    },
+    {
+      "icon": "http://pages.ctrip.com/hotel/201811/jdsc_640es_tab1.jpg",
+      "url":
+          "https://m.ctrip.com/events/jiudianshangchenghuodong.html?disable_webview_cache_key=1"
+    },
+    {
+      "icon":
+          "https://dimg03.c-ctrip.com/images/fd/tg/g1/M03/7E/19/CghzfVWw6OaACaJXABqNWv6ecpw824_C_500_280_Q90.jpg",
+      "url":
+          "https://dimg03.c-ctrip.com/images/fd/tg/g1/M03/7E/19/CghzfVWw6OaACaJXABqNWv6ecpw824_C_500_280_Q90.jpg"
+    }
+  ];
+
+  _onScroll(offset) {
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('首页'),
-      ),
-    );
+        body: Stack(
+      children: <Widget>[
+        MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollUpdateNotification &&
+                    scrollNotification.depth == 0) {
+                  //滚动并且是列表滚动的时候
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      autoplay: true,
+                      loop: true,
+                      pagination: SwiperPagination(),
+                      itemCount: _imageUrls.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imageUrls[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text('哈哈'),
+                    ),
+                  )
+                ],
+              ),
+            )),
+        Opacity(
+          opacity: appBarAlpha,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(color: Colors.white),
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text('首页'),
+              ),
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
