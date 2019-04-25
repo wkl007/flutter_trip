66cbe6dd4d0614d7aa52d19609b45117879b9216
wkl007 <499657357@qq.com>
2019-04-25 11:06:19 +0800

    app启动动态权限设置

--- android/app/src/main/java/com/example/flutter_trip/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_trip/MainActivity.java
@@ -4,6 +4,9 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import org.wkl.flutter.plugin.asr.AsrPlugin;
 
 import java.util.ArrayList;
@@ -11,16 +14,44 @@ import java.util.ArrayList;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-
 public class MainActivity extends FlutterActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         GeneratedPluginRegistrant.registerWith(this);
         registerSelfPlugin();
+        initPermission();
     }
 
     private void registerSelfPlugin() {
         AsrPlugin.registerWith(registrarFor("org.wkl.flutter.plugin.asr.AsrPlugin"));
+    }
+
+    /**
+     * android 6.0 以上需要动态申请权限
+     */
+    private void initPermission() {
+        String permissions[] = {Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+
+        ArrayList<String> toApplyList = new ArrayList<String>();
+
+        for (String perm : permissions) {
+            if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, perm)) {
+                toApplyList.add(perm);
+                //进入到这里代表没有权限.
+
+            }
+        }
+
+        String tmpList[] = new String[toApplyList.size()];
+        if (!toApplyList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, toApplyList.toArray(tmpList), 123);
+        }
     }
 }
