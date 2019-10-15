b63d5b7acd06da9ff41464c289425ed36b25e74d
wkl007 <499657357@qq.com>
2019-10-15 17:27:47 +0800

    build: ios构建

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\webProject\github\flutter_trip"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/macos/webProject/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/macos/webProject/github/flutter_trip"
+export "FLUTTER_TARGET=/Users/macos/webProject/github/flutter_trip/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=D:\flutter\bin\cache\artifacts\engine\ios"
-export "FLUTTER_BUILD_NAME=1.0.1"
-export "FLUTTER_BUILD_NUMBER=2"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/macos/webProject/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.2"
+export "FLUTTER_BUILD_NUMBER=3"
+export "TRACK_WIDGET_CREATION=true"
