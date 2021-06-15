04ed9694c3a5fab30421bfbf7d713e4678bed610
wkl007 <499657357@qq.com>
2021-06-15 22:34:34 +0800

    feat: 安卓端友盟统计集成

--- android/app/src/main/kotlin/com/wkl/flutter_trip/MainActivity.kt
+++ android/app/src/main/kotlin/com/wkl/flutter_trip/MainActivity.kt
@@ -10,20 +10,15 @@ class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         UMConfigure.init(this, "5cc2c66a570df33914000e9f", "Flutter Trip", UMConfigure.DEVICE_TYPE_PHONE, null)
-        UMConfigure.setLogEnabled(true)
-        com.umeng.umeng_common_sdk.UmengCommonSdkPlugin.setContext(this)
-        android.util.Log.i("UMLog", "onCreate@MainActivity")
     }
 
     override fun onPause() {
         super.onPause()
         MobclickAgent.onPause(this)
-        android.util.Log.i("UMLog", "onPause@MainActivity")
     }
 
     override fun onResume() {
         super.onResume()
         MobclickAgent.onResume(this)
-        android.util.Log.i("UMLog", "onResume@MainActivity")
     }
 }
